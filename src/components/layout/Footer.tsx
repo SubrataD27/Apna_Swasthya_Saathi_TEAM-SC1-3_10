a2a35c37927939461de34f71c7a7571e5a6275f8
SubrataD27 <dhibarsubrata18@gmail.com>
2025-07-25 00:53:01 +0530

    2nd push

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -65,7 +65,7 @@ const Footer = () => {
             <div className="space-y-3">
               <div className="flex items-center space-x-3">
                 <Phone className="h-4 w-4 text-blue-400" />
-                <span className="text-gray-300">+91 9876543210</span>
+                <span className="text-gray-300">+91 9XXXXXXXXX</span>
               </div>
               <div className="flex items-center space-x-3">
                 <Mail className="h-4 w-4 text-blue-400" />
